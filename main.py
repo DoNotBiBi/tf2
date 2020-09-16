aba367d6cfd3c4a7082496743545218e9743c66e
wk_ang <wangkang941218@outlook.com>
2020-09-16 09:15:56 +0800

    hello

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # 查看tensorflow版本
-    print(tf.__version__)
+    print(tf.__version__) ## 测试 ##
 
